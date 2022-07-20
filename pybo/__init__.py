a87e486ada016698a5ceb956cc5ac6d43a98a6bb
Yun Jiseong <73680499+Yunjiseong@users.noreply.github.com>
2022-07-20 17:47:58 +0900

    import 코드 수정

--- pybo/__init__.py
+++ pybo/__init__.py
@@ -1,14 +1,12 @@
 from flask import Flask
 from flask_migrate import Migrate
 from flask_sqlalchemy import SQLAlchemy
-
 import config
 
 db = SQLAlchemy()
 migrate = Migrate()
-
-
 # app = Flask(__name__)  # 플라스크 애플리케이션 생성코드, __name__에 모듈명 담김(pybo)
+
 
 def create_app():
     app = Flask(__name__)
@@ -20,18 +18,21 @@ def create_app():
     from . import models
 
     # 블루프린트
-    from .views import main_views, question_views, answer_views
+    from .views import main_views, question_views, answer_views, auth_views
     app.register_blueprint(main_views.bp)
     app.register_blueprint(question_views.bp)
     app.register_blueprint(answer_views.bp)
+    app.register_blueprint(auth_views.bp)
 
     # 필터
     from .filter import format_datetime
     app.jinja_env.filters['datetime'] = format_datetime
-
 
     # @app.route('/')  # url 매핑해 다음줄 함수 실행
     # def hello_pybo():
     #     return 'Hello, Pybo!'
 
     return app
+
+
+
