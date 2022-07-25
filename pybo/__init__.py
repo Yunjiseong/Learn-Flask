4973ad92a69be3c278d61c20c1bc020b0b87ab22
Yun Jiseong <73680499+Yunjiseong@users.noreply.github.com>
2022-07-25 09:30:53 +0900

    15일

--- pybo/__init__.py
+++ pybo/__init__.py
@@ -1,9 +1,19 @@
 from flask import Flask
 from flask_migrate import Migrate
 from flask_sqlalchemy import SQLAlchemy
+from sqlalchemy import MetaData
+
 import config
 
-db = SQLAlchemy()
+naming_convention = {
+    "ix": 'ix_%(column_0_label)s',
+    "uq": "uq_%(table_name)s_%(column_0_name)s",
+    "ck": "ck_%(table_name)s_%(column_0_name)s",
+    "fk": "fk_%(table_name)s_%(column_0_name)s_%(referred_table_name)s",
+    "pk": "pk_%(table_name)s"
+}
+
+db = SQLAlchemy(metadata=MetaData(naming_convention=naming_convention))
 migrate = Migrate()
 # app = Flask(__name__)  # 플라스크 애플리케이션 생성코드, __name__에 모듈명 담김(pybo)
 
@@ -14,7 +24,10 @@ def create_app():
 
     # ORM
     db.init_app(app)
-    migrate.init_app(app, db)
+    if app.config['SQLALCHEMY_DATABASE_URI'].startswith("sqlite"):
+        migrate.init_app(app, db, render_as_batch=True)
+    else:
+        migrate.init_app(app, db)
     from . import models
 
     # 블루프린트
