e658190b14da52dba34a23ae4e2efbb5775d1e22
Yun Jiseong <73680499+Yunjiseong@users.noreply.github.com>
2022-07-27 21:32:34 +0900

    16일차

--- pybo/forms.py
+++ pybo/forms.py
@@ -3,6 +3,10 @@ from wtforms import StringField, TextAreaField, PasswordField, EmailField
 from wtforms.validators import DataRequired, Length, EqualTo, Email
 
 
+class CommentForm(FlaskForm):
+    content = TextAreaField('내용', validators=[DataRequired()])
+
+
 class QuestionForm(FlaskForm):
     subject = StringField('제목', validators=[DataRequired('제목은 필수에요.')])
     content = TextAreaField('내용', validators=[DataRequired('내용은 필수에요.')])
